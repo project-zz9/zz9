7410fbeac6c28b8cd531c658490fc75fbcffd3c7
Vincent Kelvin <30566564+KLumy@users.noreply.github.com>
2022-11-01 20:51:41 +0900

    기본 데이터 셋팅

--- src/layers/foreground/RegistrationForeground/RegistrationForeground.tsx
+++ src/layers/foreground/RegistrationForeground/RegistrationForeground.tsx
@@ -54,11 +54,65 @@ const jsonSchema: JSONSchemaType<VisitorData> = {
       type: "string",
       nullable: true,
       formType: "date-input",
+      props: {
+        title: ["언제 방문하실 예정인가요?"],
+      },
     },
     additional: {
       type: "string",
       nullable: true,
       formType: "additional-input",
+      props: {
+        title: ["지수의 지구 방문이", "확정되었어요!"],
+        subTitle: [
+          [{ span: "이제 지수의 지구를 밝힐" }],
+          [
+            { type: "emphasis", span: "소중한 당신의 별" },
+            { span: " 을 만들어 볼게요 ✨" },
+          ],
+        ],
+        distance: {
+          label: "당신과 지수의 거리는 얼마큼 인가요?",
+          enum: [
+            {
+              value: "10cm",
+              description: "서로 안아줄 수 있는 사이",
+            },
+            {
+              value: "50cm",
+              description: "함께 걸을 수 있는 사이",
+            },
+            {
+              value: "1m",
+              description: "웃을 수 있는 사이",
+            },
+          ],
+        },
+        stars: {
+          label: "원하는 별 모양을 선택해주세요.",
+          enum: [
+            {
+              value: "star1",
+            },
+            {
+              value: "star2",
+            },
+            {
+              value: "star3",
+            },
+            {
+              value: "star4",
+            },
+            {
+              value: "star5",
+            },
+            {
+              value: "star6",
+            },
+          ],
+        },
+      },
+      options: {},
     },
   },
 };
