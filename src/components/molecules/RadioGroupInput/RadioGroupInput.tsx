951f59fee4f1073d8ce5cabff73dd93b8457d4a3
Vincent Kelvin <30566564+KLumy@users.noreply.github.com>
2022-11-01 22:50:20 +0900

    라디오그룹 컨포넌트 추가

---
+++ src/components/molecules/RadioGroupInput/RadioGroupInput.tsx
@@ -0,0 +1,73 @@
+import { Fragment } from "react";
+import { FormControlLabel, Radio, RadioGroup, SvgIcon } from "@mui/material";
+import styled from "styled-components";
+
+interface IRadioGroupInputProps {
+  Icon?: typeof SvgIcon;
+  title: string;
+  value: string;
+  setValue: (value: string | ((prev: string) => string)) => void;
+  options: {
+    label?: string;
+    value: string;
+  }[];
+}
+
+function RadioGroupInput({
+  Icon,
+  title,
+  value,
+  setValue,
+  options,
+}: IRadioGroupInputProps) {
+  const handleChange = ({ target }: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(target.value);
+  };
+  return (
+    <Fragment>
+      <TitleFrame>
+        {Icon && <Icon style={{ color: "#FF5A0D", fontSize: "1.3rem" }} />}
+        <Title>{title}</Title>
+      </TitleFrame>
+      <RadioGroupFrame>
+        <RadioGroup value={value} onChange={handleChange}>
+          {options.map(({ label, value }, index) => (
+            <FormControlLabel
+              key={`${index}::${label}::${value}`}
+              value={value}
+              control={
+                <Radio
+                  sx={{
+                    padding: "7px",
+                    color: "#000",
+                    "&.Mui-checked": {
+                      color: "#FF5A0D",
+                    },
+                  }}
+                />
+              }
+              label={label}
+            />
+          ))}
+        </RadioGroup>
+      </RadioGroupFrame>
+    </Fragment>
+  );
+}
+
+export default RadioGroupInput;
+
+const TitleFrame = styled.div`
+  margin: 10px;
+`;
+const Title = styled.div`
+  display: inline;
+  vertical-align: text-bottom;
+  font-size: 1.1rem;
+  margin-left: 7px;
+  font-weight: 600;
+`;
+
+const RadioGroupFrame = styled.div`
+  margin-left: 35px;
+`;
