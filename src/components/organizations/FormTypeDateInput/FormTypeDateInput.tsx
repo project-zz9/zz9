d2c9940b66840472d4b75437f985e54f78b5c281
Vincent Kelvin <30566564+KLumy@users.noreply.github.com>
2022-11-01 23:10:18 +0900

    데이터 입력 폼 추가

--- src/components/organizations/FormTypeDateInput/FormTypeDateInput.tsx
+++ src/components/organizations/FormTypeDateInput/FormTypeDateInput.tsx
@@ -1,12 +1,79 @@
+import { CalendarToday, Schedule } from "@mui/icons-material";
+import { Divider } from "@mui/material";
+import { Fragment, useEffect, useState } from "react";
+import styled from "styled-components";
+import MultiLineText from "~/components/molecules/MultiLineText";
+import RadioGroupInput from "~/components/molecules/RadioGroupInput";
 import { IFormTypeInputProps } from "../SchemaForm";
 
 interface IFormTypeDateInputProps<T = Record<string, string>>
   extends IFormTypeInputProps<T> {
-  props?: {};
-  options?: {};
+  props?: {
+    title: string[];
+    date: {
+      title: string;
+      options: { label: string; value: string }[];
+    };
+    time: {
+      title: string;
+      options: { label: string; value: string }[];
+    };
+  };
 }
-function FormTypeDateInput({}: IFormTypeDateInputProps) {
-  return <div>FormTypeDateInput</div>;
+function FormTypeDateInput({
+  name,
+  onChange,
+  error,
+  props,
+}: IFormTypeDateInputProps) {
+  const [date, setDate] = useState<string>("");
+  const [time, setTime] = useState<string>("");
+
+  const { title, date: dateProps, time: timeProps } = props || {};
+
+  useEffect(() => {
+    if (date && time) {
+      onChange((prev) => ({
+        ...prev,
+        [name]: `${date} ${time}`,
+      }));
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [date, time]);
+
+  return (
+    <Fragment>
+      <div>{title && <MultiLineText type="title" lines={title} />}</div>
+      <RadioGroupFrame>
+        {dateProps && (
+          <RadioGroupInput
+            title={dateProps.title}
+            Icon={CalendarToday}
+            value={date}
+            setValue={setDate}
+            options={dateProps.options}
+          />
+        )}
+      </RadioGroupFrame>
+      <Divider />
+      <RadioGroupFrame>
+        {timeProps && (
+          <RadioGroupInput
+            title={timeProps.title}
+            Icon={Schedule}
+            value={time}
+            setValue={setTime}
+            options={date ? timeProps.options : []}
+          />
+        )}
+      </RadioGroupFrame>
+    </Fragment>
+  );
 }
 
 export default FormTypeDateInput;
+
+const RadioGroupFrame = styled.div`
+  margin-top: 10px;
+  margin-bottom: 10px;
+`;
