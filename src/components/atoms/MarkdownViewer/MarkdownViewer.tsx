16cc9efd16ca55050a9a2c54985e0e659babeadf
Vincent Kelvin <30566564+KLumy@users.noreply.github.com>
2022-11-03 00:00:06 +0900

    개인정보 수집 안내 자세한 페이지 구성

--- src/components/atoms/MarkdownViewer/MarkdownViewer.tsx
+++ src/components/atoms/MarkdownViewer/MarkdownViewer.tsx
@@ -1,12 +1,42 @@
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
-
+import stringWidth from "string-width";
+import styled from "styled-components";
 interface IMarkdownViewerProps {
   markdown: string;
 }
 
 function MarkdownViewer({ markdown }: IMarkdownViewerProps) {
-  return <ReactMarkdown children={markdown} remarkPlugins={[remarkGfm]} />;
+  return (
+    <MarkdownViewerFrame>
+      <ReactMarkdown
+        children={markdown}
+        remarkPlugins={[
+          [remarkGfm, { singleTilde: false, stringLength: stringWidth }],
+        ]}
+      />
+    </MarkdownViewerFrame>
+  );
 }
 
 export default MarkdownViewer;
+
+const MarkdownViewerFrame = styled.div`
+  table {
+    width: 100%;
+
+    thead {
+      background-color: gray;
+      th {
+        min-width: 5rem;
+        font-size: 0.85rem;
+      }
+    }
+    tbody {
+      background-color: lightgray;
+      td {
+        font-size: 0.75rem;
+      }
+    }
+  }
+`;
--- src/components/organizations/ApprovePermissionModalDetailInner/ApprovePermissionModalDetailInner.tsx
+++ src/components/organizations/ApprovePermissionModalDetailInner/ApprovePermissionModalDetailInner.tsx
@@ -1,8 +1,17 @@
 import MarkdownViewer from "~/components/atoms/MarkdownViewer";
 import { PermissionPersonalData_Detail } from "~/assets/contents";
+import styled from "styled-components";
 
 function ApprovePermissionModalDetailInner() {
-  return <MarkdownViewer markdown={PermissionPersonalData_Detail} />;
+  return (
+    <MarkdownViewerFrame>
+      <MarkdownViewer markdown={PermissionPersonalData_Detail} />
+    </MarkdownViewerFrame>
+  );
 }
 
 export default ApprovePermissionModalDetailInner;
+
+const MarkdownViewerFrame = styled.div`
+  margin: 25px;
+`;
