8c18d1d2d15ecccd216d61db0b958cc0e1756604
Vincent Kelvin <30566564+KLumy@users.noreply.github.com>
2022-11-02 21:02:42 +0900

    MarkdownViewer 셋팅

--- src/components/atoms/MarkdownViewer/MarkdownViewer.tsx
+++ src/components/atoms/MarkdownViewer/MarkdownViewer.tsx
@@ -1,11 +1,12 @@
 import ReactMarkdown from "react-markdown";
+import remarkGfm from "remark-gfm";
 
 interface IMarkdownViewerProps {
   markdown: string;
 }
 
 function MarkdownViewer({ markdown }: IMarkdownViewerProps) {
-  return <ReactMarkdown children={markdown} />;
+  return <ReactMarkdown children={markdown} remarkPlugins={[remarkGfm]} />;
 }
 
 export default MarkdownViewer;
