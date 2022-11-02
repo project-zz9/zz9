88169a47f1caac4eb02634cfd6cf9d9648eb23f8
Vincent Kelvin <30566564+KLumy@users.noreply.github.com>
2022-11-02 22:53:52 +0900

    stack을 사용한 history 방식 개선

--- src/layers/modal/Modal.tsx
+++ src/layers/modal/Modal.tsx
@@ -6,13 +6,19 @@ import { modalAtom, ModalParameter, visibleAtom } from "~/stores/modal";
 function Modal() {
   const [modalData] = useAtom(modalAtom);
   const [visible, setVisible] = useAtom(visibleAtom);
-  const [historyStack, setHistoryStack] = useState<ModalParameter[]>([]);
+  const [currentModal, setCurrentModal] = useState<ModalParameter | null>(null);
+  const [history, setHistory] = useState<ModalParameter[]>([]);
+
   useEffect(() => {
-    modalData && setHistoryStack((stack) => [...stack, modalData]);
+    modalData && setHistory((history) => [...history, modalData]);
   }, [modalData]);
 
-  const { type, content, Element, onSubmit, onCancel } =
-    historyStack.at(-1) || {};
+  useEffect(() => {
+    history.length > 0 && setCurrentModal(history.at(-1) || null);
+  }, [history]);
+
+  const { type, stacked, content, Element, onSubmit, onCancel } =
+    currentModal || {};
 
   const { onCancelHandler, onSubmitHandler } = useMemo(
     () => ({
@@ -20,27 +26,27 @@ function Modal() {
         onCancel?.handler &&
           typeof onCancel.handler === "function" &&
           onCancel.handler();
-        historyStack.length > 1
-          ? setHistoryStack((stack) => stack.slice(0, -1))
-          : setVisible(false);
+        setHistory((history) => history.slice(0, -1));
+        if (history.length === 1) setVisible(false);
       },
       onSubmitHandler() {
         onSubmit?.handler &&
           typeof onSubmit.handler === "function" &&
           onSubmit.handler();
-        historyStack.length > 1
-          ? setHistoryStack((stack) => stack.slice(0, -1))
-          : setVisible(false);
+        if (!stacked) {
+          setHistory((history) => history.slice(0, -1));
+          history.length === 1 && setVisible(false);
+        }
       },
     }),
-    [historyStack, onCancel, onSubmit, setVisible]
+    [stacked, history, onCancel, onSubmit, setVisible]
   );
 
   return (
     <CustomModal
       visible={visible}
       type={type}
-      hash={JSON.stringify(historyStack.at(-1) || {})}
+      hash={JSON.stringify(currentModal || {})}
       Element={Element}
       content={content}
       onSubmit={onSubmit}
