74fb46644da169040cd245d58fe23ad7ce45bb19
Vincent Kelvin <30566564+KLumy@users.noreply.github.com>
2022-11-05 02:48:10 +0900

    불필요한 package 제거

--- src/components/atoms/EmphasisText/EmphasisText.tsx
+++ src/components/atoms/EmphasisText/EmphasisText.tsx
@@ -1,5 +1,5 @@
 import { Fragment } from "react";
-import styled, { css } from "styled-components";
+import styled from "styled-components";
 
 export type EmphasisTextForm = {
   type?: "common" | "emphasis";
