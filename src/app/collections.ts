a6e175e0539eec9b3fc81f1733b3b1a6e88174a0
Vincent Kelvin <30566564+KLumy@users.noreply.github.com>
2022-11-06 01:18:19 +0900

    Firestore API 구축, 고도화 (#18)
    * 공통 사용 코드를 API 클래스로 병합
    
    * 불필요한 파일 제거

--- src/app/collections.ts
+++
@@ -1,9 +0,0 @@
-import { firestore } from "./firesore";
-
-const VISITOR = "visitor";
-const GUESTBOOK = "guestbook";
-const SCHEDULE = "schedule";
-
-export const visitorCollection = firestore.collection(VISITOR);
-export const scheduleCollection = firestore.collection(SCHEDULE);
-export const guestbookCollection = firestore.collection(GUESTBOOK);
