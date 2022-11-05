fb9e494343797370465e0c107840e6e4250fc5b3
Vincent Kelvin <30566564+KLumy@users.noreply.github.com>
2022-11-06 01:38:01 +0900

    사전등록이 완료되면 등록 id를 프리뷰 페이지로 전달

--- src/pages/invitation-preview/invitation-preview.tsx
+++ src/pages/invitation-preview/invitation-preview.tsx
@@ -1,7 +1,9 @@
+import { useParams } from "react-router-dom";
 import MonotonicBackground from "~/layers/background/MonotonicBackground";
 import DemoForeground from "~/layers/foreground/DemoForeground";
 
 function InvitationPreview() {
+  const { uuid } = useParams();
   return (
     <>
       <DemoForeground title="InvitationPreview" color="red" />
