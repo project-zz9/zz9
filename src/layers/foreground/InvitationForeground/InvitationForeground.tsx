23af846a90a5b36072f8e0746762f3b34b046cc3
Vincent Kelvin <30566564+KLumy@users.noreply.github.com>
2022-11-07 01:54:48 +0900

    실제 데이터를 수신하도록 변경

--- src/layers/foreground/InvitationForeground/InvitationForeground.tsx
+++ src/layers/foreground/InvitationForeground/InvitationForeground.tsx
@@ -3,7 +3,7 @@ import { useQuery } from "~/hooks/useQuery";
 import ForegroundLayer from "../ForegroundLayer";
 import { useNavigate } from "react-router-dom";
 import { HOME_PATH } from "~/pages";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useAtom } from "jotai";
 import { modalControlAtom } from "~/stores/modal";
 import InvitationTabs from "~/components/organizations/InvitationTabs";
@@ -12,38 +12,31 @@ interface IInvitationForegroundProps {
   uuid: string | undefined;
 }
 
-const visitor = {
-  name: "보조개협곡",
-  phoneNumber: "010-9123-1241",
-  visitTime: "2023-04-28 14:00",
-  relationship: "10cm::card3",
-};
-
 function InvitationForeground({ uuid }: IInvitationForegroundProps) {
   const navigate = useNavigate();
   const [, setModal] = useAtom(modalControlAtom);
-  // const visitor = useQuery<VisitorData>(
-  //   { collection: "visitor", method: "get" },
-  //   uuid
-  // );
+  const visitor = useQuery<VisitorData>(
+    { collection: "visitor", method: "get" },
+    uuid
+  );
 
-  // useEffect(() => {
-  //   if (visitor === null) {
-  //     setModal({
-  //       type: "information",
-  //       content: {
-  //         title: "정보 조회 실패",
-  //         body: "사전등록 정보를 찾을 수 없습니다!",
-  //       },
-  //       onSubmit: {
-  //         label: "뒤로가기",
-  //         handler: () => {
-  //           navigate(HOME_PATH);
-  //         },
-  //       },
-  //     });
-  //   }
-  // }, [navigate, setModal, visitor]);
+  useEffect(() => {
+    if (visitor === null) {
+      setModal({
+        type: "information",
+        content: {
+          title: "정보 조회 실패",
+          body: "사전등록 정보를 찾을 수 없습니다!",
+        },
+        onSubmit: {
+          label: "뒤로가기",
+          handler: () => {
+            navigate(HOME_PATH);
+          },
+        },
+      });
+    }
+  }, [navigate, setModal, visitor]);
   return (
     <ForegroundLayer>
       <RootFrame>
