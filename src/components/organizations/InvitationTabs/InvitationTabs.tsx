8fbfcc8f9d81c5193544b8e5f052a9731b812625
Vincent Kelvin <30566564+KLumy@users.noreply.github.com>
2022-11-07 23:51:32 +0900

    css 충돌 해결

--- src/components/organizations/InvitationTabs/InvitationTabs.tsx
+++ src/components/organizations/InvitationTabs/InvitationTabs.tsx
@@ -44,7 +44,7 @@ function InvitationTabs({ uuid, visitor }: IInvitationTabsProps) {
   return (
     <Root>
       <TransitionGroup>
-        <CSSTransition key={tab} classNames="fade" timeout={350}>
+        <CSSTransition key={tab} classNames="fade-absolute" timeout={350}>
           {TabComponent && (
             <TabComponent
               uuid={uuid}
