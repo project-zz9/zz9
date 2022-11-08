8628f4e6912f3dcc4d39d5570685f36776fa3178
Vincent Kelvin <30566564+KLumy@users.noreply.github.com>
2022-11-09 02:40:06 +0900

    테스트코드 추가

--- src/app/constant.ts
+++ src/app/constant.ts
@@ -1,3 +1,8 @@
-export const MAX_VISITOR_ON_ONE_TIME = 20;
+export const MAX_VISITOR_ON_ONE_TIME = 3;
+
+export const MINUTE = 60;
+export const HOUR = 60 * MINUTE;
+export const DAY = 24 * HOUR;
+export const WEEK = 7 * DAY;
 
 export const SEPARATOR = "::";
