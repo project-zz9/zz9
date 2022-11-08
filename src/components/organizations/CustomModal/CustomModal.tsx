13e31dfc9ebbd1c420ad7d115b583f0867ceca83
Vincent Kelvin <30566564+KLumy@users.noreply.github.com>
2022-11-08 23:46:36 +0900

    임시 수정, 상태 변경이 충돌하지 않도록 수정

--- src/components/organizations/CustomModal/CustomModal.tsx
+++ src/components/organizations/CustomModal/CustomModal.tsx
@@ -95,7 +95,8 @@ function CustomModal({
 }: ICustomModalProps) {
   const [activate, setActivate] = useState<boolean>(true);
   useEffect(() => {
-    setActivate(true);
+    !activate && setActivate(true);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [hash]);
   return (
     <Dialog
