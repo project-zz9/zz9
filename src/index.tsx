42993cb0423841f1917c6dc04d003e60eac8d86c
Vincent Kelvin <30566564+KLumy@users.noreply.github.com>
2022-11-09 01:55:24 +0900

    폰트 적용 완료

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import ReactDOM from "react-dom/client";
 import "~/assets/styles/global.css";
 import "~/assets/styles/animation.css";
+import "~/assets/fonts/pretendard/pretendardvariable.css";
 import Root from "~/frames/Root";
 
 const root = ReactDOM.createRoot(
