22b492b8fd3f7fec722efc6da59825b4a92e2e36
Vincent Kelvin <30566564+KLumy@users.noreply.github.com>
2022-11-20 22:18:20 +0900

    뷰파인터 색상 변경

--- src/components/atoms/ViewFinder/ViewFinder.tsx
+++ src/components/atoms/ViewFinder/ViewFinder.tsx
@@ -18,25 +18,25 @@ function ViewFinder() {
         <path
           fill="none"
           d="M13,0 L0,0 L0,13"
-          stroke="rgba(255, 0, 0, 0.5)"
+          stroke="rgba(255, 255, 255, 0.5)"
           strokeWidth="5"
         />
         <path
           fill="none"
           d="M0,87 L0,100 L13,100"
-          stroke="rgba(255, 0, 0, 0.5)"
+          stroke="rgba(255, 255, 255, 0.5)"
           strokeWidth="5"
         />
         <path
           fill="none"
           d="M87,100 L100,100 L100,87"
-          stroke="rgba(255, 0, 0, 0.5)"
+          stroke="rgba(255, 255, 255, 0.5)"
           strokeWidth="5"
         />
         <path
           fill="none"
           d="M100,13 L100,0 87,0"
-          stroke="rgba(255, 0, 0, 0.5)"
+          stroke="rgba(255, 255, 255, 0.5)"
           strokeWidth="5"
         />
       </svg>
