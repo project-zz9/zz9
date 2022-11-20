b12b1af695032e84b42ce3260b0c91466e9abc40
Vincent Kelvin <30566564+KLumy@users.noreply.github.com>
2022-11-20 15:37:47 +0900

    MarkdownViewer lazy import 적용

--- src/components/organizations/ApprovePermissionModalDetailInner/ApprovePermissionModalDetailInner.tsx
+++ src/components/organizations/ApprovePermissionModalDetailInner/ApprovePermissionModalDetailInner.tsx
@@ -1,11 +1,15 @@
-import MarkdownViewer from "~/components/atoms/MarkdownViewer";
 import { PermissionPersonalData_Detail } from "~/assets/contents";
 import styled from "styled-components";
+import { lazy, Suspense } from "react";
+
+const MarkdownViewer = lazy(() => import("~/components/atoms/MarkdownViewer"));
 
 function ApprovePermissionModalDetailInner() {
   return (
     <MarkdownViewerFrame>
-      <MarkdownViewer markdown={PermissionPersonalData_Detail} />
+      <Suspense fallback={<div>Loading...</div>}>
+        <MarkdownViewer markdown={PermissionPersonalData_Detail} />
+      </Suspense>
     </MarkdownViewerFrame>
   );
 }
