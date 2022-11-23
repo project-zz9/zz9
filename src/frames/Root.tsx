7fcb53bc5b01e9a9cb665643f22ac125392fcdde
Vincent Kelvin <30566564+KLumy@users.noreply.github.com>
2022-11-23 20:37:32 +0900

    resizeEventHandler 추가

--- src/frames/Root.tsx
+++ src/frames/Root.tsx
@@ -1,8 +1,16 @@
+import { useEffect } from "react";
 import { HashRouter as Router } from "react-router-dom";
 import Modal from "~/layers/modal";
 import Application from "./Application";
 
 function Root() {
+  useEffect(() => {
+    resizeEventHandler();
+    window.addEventListener("resize", resizeEventHandler);
+    return () => {
+      window.removeEventListener("resize", resizeEventHandler);
+    };
+  }, []);
   return (
     <>
       <Router>
@@ -14,3 +22,8 @@ function Root() {
 }
 
 export default Root;
+
+function resizeEventHandler() {
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty("--vh", `${vh}px`);
+}
